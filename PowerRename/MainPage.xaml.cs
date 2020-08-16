b3e9e6e76918b78278699ce66ba5ac9f7ac2bb83
Niels Laute <niels.laute@live.nl>
2020-08-16 22:34:14 +0200

    Updated UI and used datagrid

--- PowerRename/MainPage.xaml.cs
+++ PowerRename/MainPage.xaml.cs
@@ -24,6 +24,7 @@ namespace PowerRename
     public sealed partial class MainPage : Page
     {
         public ObservableCollection<FileName> FileNames { get; set; }
+        public ObservableCollection<RegExShortcut> RegExShortcuts { get; set; }
 
         public MainPage()
         {
@@ -39,14 +40,34 @@ namespace PowerRename
                 new FileName() { Original = "Surface 6_wallpaper.jpg", Renamed = "6_wallpaper.jpg" },
                 new FileName() { Original = "Surface 7_wallpaper.jpg", Renamed = "7_wallpaper.jpg" }
             };
+
+            RegExShortcuts = new ObservableCollection<RegExShortcut>
+            {
+                new RegExShortcut() { Code = "\\.", Description = "Matches any character" },
+                new RegExShortcut() { Code = "\\d", Description = "Any digit, short for [0-9]" },
+                new RegExShortcut() { Code = "\\D", Description = "A non-digit, short for [^0-9]" },
+                new RegExShortcut() { Code = "\\w", Description = "A non-whitespace character, short for [^\\s]" },
+                new RegExShortcut() { Code = "\\S", Description = "A word character, short for [a-zA-Z_0-9]" },
+                new RegExShortcut() { Code = "\\S+", Description = "Several non-whitespace characters" },
+                new RegExShortcut() { Code = "\\b", Description = "Matches a word boundary where a word character is [a-zA-Z0-9_]." },
+            };
         }
 
-   
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            ReplaceWidthTextBox.Text = ReplaceWidthTextBox.Text + " {" + ((RegExShortcut)e.ClickedItem).Code + "}";
+        }
     }
 
     public class FileName
     {
         public string Original { get; set; }
         public string Renamed { get; set; }
+    }
+
+    public class RegExShortcut
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
     }
 }

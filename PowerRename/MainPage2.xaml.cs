74b31c237745f6c5382d8c212471d8f4c11e26ef
Niels Laute <niels9001@hotmail.com>
2021-09-11 20:47:00 +0200

    Added additional page

---
+++ PowerRename/MainPage2.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PowerRename
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage2 : Page
+    {
+        public ObservableCollection<ExplorerItem> ExplorerItems { get; set; }
+        public ObservableCollection<RegExShortcut> SearchRegExShortcuts { get; set; }
+        public ObservableCollection<RegExShortcut> FileRegExShortcuts { get; set; }
+
+        public MainPage2()
+        {
+            this.InitializeComponent();
+
+            List<ExplorerItem> SubItems = new List<ExplorerItem>();
+            SubItems.Add(new ExplorerItem()
+            {
+                Original = "Surface 15_wallpaper.jpg",
+                Renamed = "15_wallpaper.jpg",
+                Type = ExplorerItemType.File
+            });
+            SubItems.Add(new ExplorerItem()
+            {
+                Original = "Surface 16_wallpaper.jpg",
+                Renamed = "16_wallpaper.jpg",
+                Type = ExplorerItemType.File
+            });
+
+            ExplorerItems = new ObservableCollection<ExplorerItem>
+            {
+                new ExplorerItem() { Original = "Surface 1_wallpaper.jpg", Renamed = "1_wallpaper.jpg", Type = ExplorerItemType.Folder, Children = SubItems },
+                new ExplorerItem() { Original = "Surface 2_wallpaper.jpg", Renamed = "2_wallpaper.jpg", Type = ExplorerItemType.File },
+                new ExplorerItem() { Original = "Surface 3_wallpaper.jpg", Renamed = "3_wallpaper.jpg", Type = ExplorerItemType.File },
+                new ExplorerItem() { Original = "Surface 4_wallpaper.jpg", Renamed = "4_wallpaper.jpg", Type = ExplorerItemType.Folder, Children = SubItems },
+                new ExplorerItem() { Original = "Surface 5_wallpaper.jpg", Renamed = "5_wallpaper.jpg", Type = ExplorerItemType.File },
+                new ExplorerItem() { Original = "Surface 6_wallpaper.jpg", Renamed = "6_wallpaper.jpg", Type = ExplorerItemType.Folder, Children = SubItems },
+                new ExplorerItem() { Original = "Surface 7_wallpaper.jpg", Renamed = "7_wallpaper.jpg", Type = ExplorerItemType.File },
+            };
+
+            SearchRegExShortcuts = new ObservableCollection<RegExShortcut>
+            {
+                new RegExShortcut() { Code = "\\.", Description = "Matches any character" },
+                new RegExShortcut() { Code = "\\d", Description = "Any digit, short for [0-9]" },
+                new RegExShortcut() { Code = "\\D", Description = "A non-digit, short for [^0-9]" },
+                new RegExShortcut() { Code = "\\w", Description = "A non-whitespace character, short for [^\\s]" },
+                new RegExShortcut() { Code = "\\S", Description = "A word character, short for [a-zA-Z_0-9]" },
+                new RegExShortcut() { Code = "\\S+", Description = "Several non-whitespace characters" },
+                new RegExShortcut() { Code = "\\b", Description = "Matches a word boundary where a word character is [a-zA-Z0-9_]." },
+            };
+
+            FileRegExShortcuts = new ObservableCollection<RegExShortcut>
+            {
+                new RegExShortcut() { Code = "$YYYY", Description = "Year" },
+               new RegExShortcut() { Code = "$MM", Description = "Month" },
+                 new RegExShortcut() { Code = "$DD", Description = "Day" },
+                   new RegExShortcut() { Code = "$hh", Description = "Hours" },
+                    new RegExShortcut() { Code = "$mm", Description = "Minutes" },
+                        new RegExShortcut() { Code = "$ss", Description = "Seconds" },
+                    new RegExShortcut() { Code = "$fff", Description = "Milliseconds" }
+            };
+        }
+
+        
+    }
+
+    public class FileName
+    {
+        public string Original { get; set; }
+        public string Renamed { get; set; }
+    }
+
+    public class RegExShortcut
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+    }
+}

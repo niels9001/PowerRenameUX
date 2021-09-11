90c936df3e549e292290a9b0d678e7ac04ab1feb
Niels Laute <niels9001@hotmail.com>
2021-09-11 21:00:05 +0200

    Added things

--- PowerRename/MainPage.xaml.cs
+++ PowerRename/MainPage.xaml.cs
@@ -24,8 +24,6 @@ namespace PowerRename
     public sealed partial class MainPage : Page
     {
         public ObservableCollection<ExplorerItem> ExplorerItems { get; set; }
-        public ObservableCollection<RegExShortcut> SearchRegExShortcuts { get; set; }
-        public ObservableCollection<RegExShortcut> FileRegExShortcuts { get; set; }
 
         public MainPage()
         {
@@ -55,42 +53,6 @@ namespace PowerRename
                 new ExplorerItem() { Original = "Surface 6_wallpaper.jpg", Renamed = "6_wallpaper.jpg", Type = ExplorerItemType.Folder, Children = SubItems },
                 new ExplorerItem() { Original = "Surface 7_wallpaper.jpg", Renamed = "7_wallpaper.jpg", Type = ExplorerItemType.File },
             };
-
-            SearchRegExShortcuts = new ObservableCollection<RegExShortcut>
-            {
-                new RegExShortcut() { Code = "\\.", Description = "Matches any character" },
-                new RegExShortcut() { Code = "\\d", Description = "Any digit, short for [0-9]" },
-                new RegExShortcut() { Code = "\\D", Description = "A non-digit, short for [^0-9]" },
-                new RegExShortcut() { Code = "\\w", Description = "A non-whitespace character, short for [^\\s]" },
-                new RegExShortcut() { Code = "\\S", Description = "A word character, short for [a-zA-Z_0-9]" },
-                new RegExShortcut() { Code = "\\S+", Description = "Several non-whitespace characters" },
-                new RegExShortcut() { Code = "\\b", Description = "Matches a word boundary where a word character is [a-zA-Z0-9_]." },
-            };
-
-            FileRegExShortcuts = new ObservableCollection<RegExShortcut>
-            {
-                new RegExShortcut() { Code = "$YYYY", Description = "Year" },
-               new RegExShortcut() { Code = "$MM", Description = "Month" },
-                 new RegExShortcut() { Code = "$DD", Description = "Day" },
-                   new RegExShortcut() { Code = "$hh", Description = "Hours" },
-                    new RegExShortcut() { Code = "$mm", Description = "Minutes" },
-                        new RegExShortcut() { Code = "$ss", Description = "Seconds" },
-                    new RegExShortcut() { Code = "$fff", Description = "Milliseconds" }
-            };
         }
-
-        
-    }
-
-    public class FileName
-    {
-        public string Original { get; set; }
-        public string Renamed { get; set; }
-    }
-
-    public class RegExShortcut
-    {
-        public string Code { get; set; }
-        public string Description { get; set; }
     }
 }
